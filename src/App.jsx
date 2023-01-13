9f3b48b3debb672c95cb0c2c0876d72d4b5fc44e
filipzlatojevic <ficozlatojevic@gmail.com>
2023-01-13 22:14:58 +0100

    chore(project): Initial project.

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './app.css';
+
+const App = () => <h1 className="text-primary text-orange text-4xl font-bold">Hello world! I am using React</h1>;
+
+export default App;
---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx}', './public/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        oceanBlue: '#2196f3',
+        btnGreen: '#53C247',
+        btnHoverGreen: '#2CB03F',
+        lightGreen: '#BFEDC8',
+        orange: '#e4b641',
+        lightOrange: '#F7EAD4',
+      },
+    },
+  },
+  plugins: [],
+};
